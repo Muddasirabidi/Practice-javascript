fa3ea3fe81b46570a432230e5ebb19affe161833
Syed Muddasir Abidi <124608857+Muddasirabidi@users.noreply.github.com>
2026-03-11 04:22:51 +0000

    Primitive and Non-primitive datatype

--- Basics/datatype.js
+++ Basics/datatype.js
@@ -14,7 +14,18 @@ const age = 22
 const username=Symbol("mud@gmail.com")
 let password; // did not use const since it requires initialization of variables and i want to show this variable as of undefined datatype.
 const isloggedin = false
-const username2=Symbol("mud@gmail.com") 
+const username2=Symbol("mud@gmail.com")
+let currentrole = null
+const bignumber= 345588399376255171n
+
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(username));
+console.log(typeof(password));
+console.log(typeof(isloggedin));
+console.log(typeof(currentrole));
+console.log(typeof(bignumber));
+
 
 //both variables holds same value but still shows
 // false as a result because 
@@ -22,10 +33,26 @@ const username2=Symbol("mud@gmail.com")
 
 console.log(username == username2); 
 
-let currentrole = null
-
-
-
 // Reference Type (Non Primitive) are of three types
 
-// Arrays, Objects, Functions
+// Arrays: list of values
+// Objects: key-value pairs
+// Functions
+
+
+const mycars=["mclare", "artura", "speedtail"]
+const details={
+    country: "Pakistan",
+    city: "Karachi"
+}
+
+const myfunc= function(){
+    console.log('Hello from this part of the World!');
+    
+}
+
+// The return type of non-primitive variables
+// are 'object'.
+console.log(typeof(mycars));
+console.log(typeof(details));
+console.log(typeof(myfunc));
