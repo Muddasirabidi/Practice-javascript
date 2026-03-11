074df6e025ae4439d5d51c3d6b0db41c87c47538
Syed Muddasir Abidi <124608857+Muddasirabidi@users.noreply.github.com>
2026-03-11 02:59:06 +0000

    comparison operations on null

--- Basics/test.js
+++ Basics/test.js
@@ -3,5 +3,14 @@
 let name= 'Muddasir'
 const lname= ' Abidi'
 
-console.log(name + lname);
+// console.log(name + lname);
 
+// Understanding operations on Null datatype
+
+console.log(null > 0);
+console.log(null < 0);
+console.log(null >= 0);
+console.log(null <= 0);
+console.log(null == 0);
+console.log(null != 0);
+console.log(null === 0);
